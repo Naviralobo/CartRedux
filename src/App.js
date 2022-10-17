552e9d377e1052dd2a819c889504d5cfa9150414
Naviralobo <majel.josephine>
2022-10-17 17:54:52 +0530

    fixing cart problems

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment } from "react";
 import { useEffect } from "react";
-import { uiActions } from "./store/uiSlice";
+import { sendCartData, fetchCartData } from "./store/cartActions";
 import Notification from "./components/UI/Notification";
 
 let isInitial = true;
@@ -13,47 +13,20 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
-  useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "sending",
-          message: "sending cart data",
-        })
-      );
-      const response = await fetch(
-        "https://redux-cart-cb2da-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed..");
-      }
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "success",
-          message: "sent cart data successfully",
-        })
-      );
-    };
 
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((err) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "sending cart data",
-        })
-      );
-    });
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
   return (
     <Fragment>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -5,12 +5,18 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -28,6 +34,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id); //filter creates a new array with items that doesnt haev the id of the item.id(i.e except that item that has this as id)
       } else {
